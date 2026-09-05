1e53f764f4c6d2b7c58ec759cf2f5b83ebdc8389
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-09-06 00:41:42 +0100

    test(core): exercise rejecting replacement processor

--- packages/agents-core/test/tracingProcessorReplacementRejections.test.ts
+++ packages/agents-core/test/tracingProcessorReplacementRejections.test.ts
@@ -8,12 +8,6 @@ import {
 import type { Span } from '../src/tracing/spans';
 import { Trace } from '../src/tracing/traces';
 
-function handledRejection(error: Error): Promise<void> {
-  const promise = Promise.reject(error);
-  void promise.catch(() => {});
-  return promise;
-}
-
 class BaseProcessor implements TracingProcessor {
   tracesStarted: Trace[] = [];
 
@@ -34,7 +28,7 @@ class BaseProcessor implements TracingProcessor {
 
 class RejectingShutdownProcessor extends BaseProcessor {
   shutdown(): Promise<void> {
-    return handledRejection(new Error('shutdown failed'));
+    return Promise.reject(new Error('shutdown failed'));
   }
 }
 
